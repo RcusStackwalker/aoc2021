2de072de711cc506bab502ade615031914e5dc55
Aleksei Markelov <markelovai@gmail.com>
2021-12-01 10:11:04 +0300

    day1 task2

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,8 +1,28 @@
 use itertools::Itertools;
 use std::fs;
+use std::ops::Add;
+
+fn number_of_inc_from_iter<I>(it: I) -> usize
+where
+    I: Sized + Iterator,
+    <I as Iterator>::Item: Clone + PartialOrd,
+{
+    it.tuple_windows().filter(|(l, r)| l < r).count()
+}
 
 fn number_of_inc<T: Ord>(data: &Vec<T>) -> usize {
-    data.iter().tuple_windows().filter(|(l, r)| l < r).count()
+    number_of_inc_from_iter(data.iter())
+}
+
+fn number_of_inc_sliding_window3<T>(data: &Vec<T>) -> usize
+where
+    T: Ord + Add<Output = T> + Clone,
+{
+    number_of_inc_from_iter(
+        data.iter()
+            .tuple_windows()
+            .map(|(a, b, c)| a.clone() + b.clone() + c.clone()),
+    )
 }
 
 #[test]
@@ -10,7 +30,7 @@ fn task1_example_data() {
     let data = fs::read_to_string("src/day1/task1_example_data.txt").expect("input data missing");
     let values = Vec::from_iter(data.lines().map(|l| l.parse::<usize>().unwrap()));
     let result = number_of_inc(&values);
-    println!("{}", result);
+    println!("D1T1E {}", result);
     assert_eq!(result, 7);
 }
 
@@ -19,6 +39,24 @@ fn task1_puzzle() {
     let data = fs::read_to_string("src/day1/task1_puzzle_data.txt").expect("input data missing");
     let values = Vec::from_iter(data.lines().map(|l| l.parse::<usize>().unwrap()));
     let result = number_of_inc(&values);
-    println!("{}", result);
+    println!("D1T1P {}", result);
     assert_eq!(result, 1475);
+}
+
+#[test]
+fn task2_example_data() {
+    let data = fs::read_to_string("src/day1/task1_example_data.txt").expect("input data missing");
+    let values = Vec::from_iter(data.lines().map(|l| l.parse::<usize>().unwrap()));
+    let result = number_of_inc_sliding_window3(&values);
+    println!("D1T2E {}", result);
+    assert_eq!(result, 5);
+}
+
+#[test]
+fn task2_puzzle_data() {
+    let data = fs::read_to_string("src/day1/task1_puzzle_data.txt").expect("input data missing");
+    let values = Vec::from_iter(data.lines().map(|l| l.parse::<usize>().unwrap()));
+    let result = number_of_inc_sliding_window3(&values);
+    println!("D1T2P {}", result);
+    assert_eq!(result, 1516);
 }
