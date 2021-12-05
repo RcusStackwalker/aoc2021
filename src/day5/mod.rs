f27827c949e1ccd94a123f3a4461858313258507
Aleksei Markelov <markelovai@gmail.com>
2021-12-05 17:58:36 +0300

    day5 task2

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -1,10 +1,11 @@
+use std::cmp::Ordering;
 use itertools::Itertools;
 use crate::utils;
 
 #[derive(Debug,Copy,Clone, Eq, PartialEq, Hash)]
 struct Point {
-    x: usize,
-    y: usize
+    x: isize,
+    y: isize
 }
 
 #[derive(Debug,Copy,Clone)]
@@ -18,7 +19,7 @@ type PointMap = std::collections::HashMap<Point, usize>;
 fn read_file_into_vector(path: &str) -> Vec<Line> {
     utils::read_file_into_vector(path, |l| {
         let mut points = l.split(" -> ").map(|s| {
-            let mut pair = s.split(',').map(|x| { x.parse::<usize>().unwrap()});
+            let mut pair = s.split(',').map(|x| { x.parse::<isize>().unwrap()});
             Point {
                 x: pair.next().unwrap(),
                 y: pair.next().unwrap(),
@@ -37,29 +38,23 @@ fn add_point(map: &mut PointMap, p: Point) {
 fn analyze(values: Vec<Line>) -> usize {
     let mut map = PointMap::new();
     values.iter().for_each(|l| {
-        if l.p1.x == l.p2.x {
-            if l.p1.y < l.p2.y {
-                l.p1.y..=l.p2.y
-            } else {
-                l.p2.y..=l.p1.y
-            }.for_each(|y| {
-                add_point(&mut map, Point {
-                    x: l.p1.x,
-                    y
-                })
-            });
-        } else if l.p1.y == l.p2.y {
-            if l.p1.x < l.p2.x {
-                l.p1.x..=l.p2.x
-            } else {
-                l.p2.x..=l.p1.x
-            }.for_each(|x| {
-                add_point(&mut map, Point {
-                    x,
-                    y: l.p1.y
-                })
-            });
+        let dx = match l.p1.x.cmp(&l.p2.x) {
+            Ordering::Less => 1,
+            Ordering::Greater => -1,
+            Ordering::Equal => 0,
+        };
+        let dy = match l.p1.y.cmp(&l.p2.y) {
+            Ordering::Less => 1,
+            Ordering::Greater => -1,
+            Ordering::Equal => 0,
+        };
+        let mut p = l.p1;
+        while p != l.p2 {
+            add_point(&mut map, p);
+            p.x += dx;
+            p.y += dy;
         }
+        add_point(&mut map, l.p2);
     });
     let result = map.iter().filter(|(_, &count)| {
         count > 1
@@ -91,5 +86,28 @@ fn task1_puzzle() {
 fn task1_puzzle_bench(b: &mut test::Bencher) {
     b.iter(|| {
         task1_puzzle();
+    });
+}
+
+#[test]
+fn task2_example() {
+    let values = read_file_into_vector("src/day5/example.txt");
+    let result = analyze(values);
+    println!("D5T2E {}", result);
+    assert_eq!(result, 12);
+}
+
+#[test]
+fn task2_puzzle() {
+    let values = read_file_into_vector("src/day5/input.txt");
+    let result = analyze(values);
+    println!("D5T2P {}", result);
+    assert_eq!(result, 19258);
+}
+
+#[bench]
+fn task2_puzzle_bench(b: &mut test::Bencher) {
+    b.iter(|| {
+        task2_puzzle();
     });
 }
