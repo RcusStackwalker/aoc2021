92004706a3640ca8cb189f973c53d14e2d96ce72
Aleksei Markelov <markelovai@gmail.com>
2021-12-07 10:20:33 +0300

    day7 task2

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -9,28 +9,43 @@ fn read_file_into_vector(path: &str) -> Vec<isize> {
         .collect()
 }
 
-fn fuel_needed(values: &Vec<isize>, pos: &isize) -> isize {
+fn fuel_needed<F>(values: &Vec<isize>, pos: &isize, burn_fn: &F) -> isize
+where
+    F: Fn(isize) -> isize,
+{
     values
         .iter()
         .map(|x| if x < pos { pos - x } else { x - pos })
+        .map(burn_fn)
         .sum()
 }
 
-fn least_fuel(values: Vec<isize>) -> isize {
+fn least_fuel<F>(values: Vec<isize>, burn_fn: F) -> isize
+where
+    F: Fn(isize) -> isize,
+{
     let minmax = values.iter().minmax();
     match minmax {
         MinMaxResult::MinMax(min, max) => (min.clone()..=max.clone())
-            .map(|x| fuel_needed(&values, &x))
+            .map(|x| fuel_needed(&values, &x, &burn_fn))
             .min()
             .unwrap(),
         _ => 0,
     }
 }
 
+fn linear_burn(delta: isize) -> isize {
+    delta
+}
+
+fn progressive_burn(delta: isize) -> isize {
+    delta * (delta + 1) / 2
+}
+
 #[test]
 fn task1_example() {
     let values = read_file_into_vector("src/day7/example.txt");
-    let result = least_fuel(values);
+    let result = least_fuel(values, linear_burn);
     println!("D7T1E {}", result);
     assert_eq!(result, 37);
 }
@@ -38,7 +53,7 @@ fn task1_example() {
 #[test]
 fn task1_puzzle() {
     let values = read_file_into_vector("src/day7/input.txt");
-    let result = least_fuel(values);
+    let result = least_fuel(values, linear_burn);
     println!("D7T1P {}", result);
     assert_eq!(result, 333755);
 }
@@ -47,5 +62,28 @@ fn task1_puzzle() {
 fn task1_puzzle_bench(b: &mut test::Bencher) {
     b.iter(|| {
         task1_puzzle();
+    });
+}
+
+#[test]
+fn task2_example() {
+    let values = read_file_into_vector("src/day7/example.txt");
+    let result = least_fuel(values, progressive_burn);
+    println!("D7T2E {}", result);
+    assert_eq!(result, 168);
+}
+
+#[test]
+fn task2_puzzle() {
+    let values = read_file_into_vector("src/day7/input.txt");
+    let result = least_fuel(values, progressive_burn);
+    println!("D7T2P {}", result);
+    assert_eq!(result, 94017638);
+}
+
+#[bench]
+fn task2_puzzle_bench(b: &mut test::Bencher) {
+    b.iter(|| {
+        task2_puzzle();
     });
 }
