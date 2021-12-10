0e2dd8001a4fea7950e574a60ad622ff7bb2160c
Aleksei Markelov <markelovai@gmail.com>
2021-12-10 10:02:38 +0300

    day10 task2

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -1,45 +1,44 @@
 use crate::utils;
+use itertools::Itertools;
 
-static POINTS: [(char, usize); 4] = [
-    (')', 3_usize),
-    (']', 57_usize),
-    ('}', 1197_usize),
-    ('>', 25137_usize),
-];
+enum ParseResult {
+    IllegalCharacter(char),
+    Incomplete(Vec<char>),
+}
 
 fn read_file_into_vector(path: &str) -> Vec<String> {
     utils::read_file_into_vector(path, |s| s.to_owned())
 }
 
-fn first_illegal_character(input: &String) -> Option<char> {
+fn parse(input: &String) -> ParseResult {
     let mut stack = Vec::with_capacity(input.len());
     for c in input.chars() {
         match c {
             '(' | '[' | '{' | '<' => stack.push(c),
             ')' => {
                 if stack.last() != Some(&'(') {
-                    return Some(c);
+                    return ParseResult::IllegalCharacter(c);
                 } else {
                     stack.pop();
                 }
             }
             ']' => {
                 if stack.last() != Some(&'[') {
-                    return Some(c);
+                    return ParseResult::IllegalCharacter(c);
                 } else {
                     stack.pop();
                 }
             }
             '}' => {
                 if stack.last() != Some(&'{') {
-                    return Some(c);
+                    return ParseResult::IllegalCharacter(c);
                 } else {
                     stack.pop();
                 }
             }
             '>' => {
                 if stack.last() != Some(&'<') {
-                    return Some(c);
+                    return ParseResult::IllegalCharacter(c);
                 } else {
                     stack.pop();
                 }
@@ -47,24 +46,66 @@ fn first_illegal_character(input: &String) -> Option<char> {
             _ => panic!("Invalid character"),
         }
     }
-    None
+    ParseResult::Incomplete(stack)
 }
 
 fn score_illegal_character(input: char) -> usize {
+    static POINTS: [(char, usize); 4] = [
+        (')', 3_usize),
+        (']', 57_usize),
+        ('}', 1197_usize),
+        ('>', 25137_usize),
+    ];
+
     POINTS
         .iter()
         .find_map(|&(c, s)| if c == input { Some(s) } else { None })
         .unwrap_or_default()
 }
 
+fn score_incomplete(prev: usize, input: &char) -> usize {
+    static POINTS: [(char, usize); 4] = [
+        ('(', 1_usize),
+        ('[', 2_usize),
+        ('{', 3_usize),
+        ('<', 4_usize),
+    ];
+    prev * 5
+        + POINTS
+            .iter()
+            .find_map(|&(c, s)| if &c == input { Some(s) } else { None })
+            .unwrap_or_default()
+}
+
 fn syntax_error_score(input: Vec<String>) -> usize {
     input
         .iter()
-        .filter_map(first_illegal_character)
+        .filter_map(|s| {
+            if let ParseResult::IllegalCharacter(c) = parse(s) {
+                Some(c)
+            } else {
+                None
+            }
+        })
         .map(score_illegal_character)
         .sum()
 }
 
+fn completion_score(input: Vec<String>) -> usize {
+    let results = input
+        .iter()
+        .filter_map(|s| {
+            if let ParseResult::Incomplete(stack) = parse(s) {
+                Some(stack)
+            } else {
+                None
+            }
+        })
+        .map(|stack| stack.iter().rev().fold(0_usize, score_incomplete))
+        .sorted()
+        .collect_vec();
+    results[results.len() / 2]
+}
 #[test]
 fn test_scoring() {
     assert_eq!(score_illegal_character(')'), 3_usize);
@@ -93,5 +134,28 @@ fn task1_puzzle() {
 fn task1_puzzle_bench(b: &mut test::Bencher) {
     b.iter(|| {
         task1_puzzle();
+    });
+}
+
+#[test]
+fn task2_example() {
+    let values = read_file_into_vector("src/day10/example.txt");
+    let result = completion_score(values);
+    println!("D10T1E {}", result);
+    assert_eq!(result, 288957);
+}
+
+#[test]
+fn task2_puzzle() {
+    let values = read_file_into_vector("src/day10/input.txt");
+    let result = completion_score(values);
+    println!("D10T1P {}", result);
+    assert_eq!(result, 3015539998);
+}
+
+#[bench]
+fn task2_puzzle_bench(b: &mut test::Bencher) {
+    b.iter(|| {
+        task2_puzzle();
     });
 }
