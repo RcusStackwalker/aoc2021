c702c214d66139cf33d5cc9ef4a7ca5109f27a14
Aleksei Markelov <markelovai@gmail.com>
2021-12-12 18:41:41 +0300

    day12: performance optimizations. we don't actually need to store full path, only the last element

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -2,25 +2,27 @@ use crate::utils;
 use itertools::Itertools;
 use std::collections::HashMap;
 
-#[derive(Clone, Eq, PartialEq, Hash, Debug)]
+type NodeIndex = u8;
+
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
 enum Point {
     Start,
     End,
-    Small(usize),
-    Big(usize),
+    Small(NodeIndex),
+    Big(NodeIndex),
 }
 type Edge = (Point, Point);
 
-fn classify(s: &str, map: &mut HashMap<String, usize>) -> Point {
+fn classify(s: &str, map: &mut HashMap<String, NodeIndex>) -> Point {
     if s.chars().next().unwrap().is_uppercase() {
-        let len = map.len();
+        let len = map.len() as NodeIndex;
         Point::Big(*map.entry(s.to_owned()).or_insert(len))
     } else {
         match s {
             "start" => Point::Start,
             "end" => Point::End,
             _ => {
-                let len = map.len();
+                let len = map.len() as NodeIndex;
                 Point::Small(*map.entry(s.to_owned()).or_insert(len))
             }
         }
@@ -39,50 +41,64 @@ fn read_file_into_vector(path: &str) -> Vec<Edge> {
 
 #[derive(Clone)]
 struct Path {
-    points: Vec<Point>,
+    //points: Vec<Point>,
+    last: Point,
     map: HashMap<Point, usize>,
+    max_small_count: usize,
 }
 
 impl Path {
     fn new() -> Path {
         let mut ret = Path {
-            points: vec![],
-            map: HashMap::new(),
+            //points: Vec::with_capacity(16),
+            last: Point::Start,
+            map: HashMap::with_capacity(16),
+            max_small_count: 0,
         };
-        ret.push(Point::Start);
+        //ret.push(Point::Start);
         ret
     }
 
     fn push(&mut self, p: Point) -> () {
-        *self.map.entry(p.clone()).or_default() += 1;
-        self.points.push(p);
+        let entry = self.map.entry(p.clone()).or_default();
+        *entry += 1;
+        if let Point::Small(_) = p {
+            self.max_small_count = self.max_small_count.max(*entry);
+        }
+        //self.points.push(p);
+        self.last = p;
     }
 
-    fn count(&self, p: &Point) -> usize {
-        self.map.get(p).unwrap_or(&0_usize).clone()
+    fn count(&self, p: Point) -> usize {
+        self.map.get(&p).unwrap_or(&0_usize).clone()
     }
 
-    fn last(&self) -> &Point {
-        self.points.last().unwrap()
+    fn last(&self) -> Point {
+        //self.points.last().unwrap()
+        self.last
     }
 
     fn finished(&self) -> bool {
-        self.last() == &Point::End
+        self.last == Point::End
     }
 }
 
-fn small_once(path: &Path, point: &Point) -> bool {
+fn small_once(path: &Path, point: Point) -> bool {
     path.count(point) == 0
 }
 
-fn small_twice(path: &Path, point: &Point) -> bool {
+fn small_twice(path: &Path, point: Point) -> bool {
     if path.count(point) == 0 {
         return true;
     }
+    path.max_small_count < 2
+    /*
     path.map.iter().all(|(point, count)| match point {
         Point::Small(_) => *count < 2,
         _ => true,
     })
+
+     */
 }
 
 fn build_path<'a, F>(
@@ -91,18 +107,18 @@ fn build_path<'a, F>(
     small_rule: &'a F,
 ) -> impl Iterator<Item = Path> + 'a
 where
-    F: Fn(&Path, &Point) -> bool,
+    F: Fn(&Path, Point) -> bool,
 {
     edges
-        .get(path.last())
+        .get(&path.last())
         .unwrap()
         .iter()
-        .filter_map(|dst| match dst {
+        .filter_map(|&dst| match &dst {
             Point::Start => None,
-            Point::End | Point::Big(_) => Some(dst.clone()),
+            Point::End | Point::Big(_) => Some(dst),
             Point::Small(_) => {
                 if small_rule(path, dst) {
-                    Some(dst.clone())
+                    Some(dst)
                 } else {
                     None
                 }
@@ -117,7 +133,7 @@ where
 
 fn path_count<F>(v: Vec<Edge>, small_rule: F) -> usize
 where
-    F: Fn(&Path, &Point) -> bool,
+    F: Fn(&Path, Point) -> bool,
 {
     let mut edges = HashMap::new();
     v.into_iter().for_each(|(p1, p2)| {
@@ -130,12 +146,17 @@ where
     });
     let mut paths = vec![Path::new()];
     let mut finished_paths = 0;
+    let mut max_len = 0;
     while paths.len() != 0 {
-        let new_paths = paths
+        paths = paths
             .iter()
             .flat_map(|path| build_path(&edges, path, &small_rule))
             .filter(|path| {
                 if path.finished() {
+                    // if max_len < path.points.len() {
+                    //     max_len = path.points.len();
+                    //     eprintln!("{:?}", path.points);
+                    // }
                     finished_paths += 1;
                     false
                 } else {
@@ -143,7 +164,6 @@ where
                 }
             })
             .collect_vec();
-        paths = new_paths;
     }
     finished_paths
 }
